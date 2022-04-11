63f606083a4e37180d63febc63ffa1af10ebd227
TERESIA012 <kingoriteresia@gmaile.com>
2022-04-11 13:38:12 +0300

    Add Firebase in Services

--- src/app/services/moviesapi.service.ts
+++ src/app/services/moviesapi.service.ts
@@ -1,35 +1,44 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment'
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesapiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient:HttpClient){}
+  baseUrl = 'https://imdb-api.com/en/API/';
 
-  baseUrl="https://imdb-api.com/en/API/"
-
-  getUpComingMovies():Observable<any>{
-    return this.httpClient.get(this.baseUrl+'ComingSoon/' + environment.apiKey)
-
+  getUpComingMovies(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'ComingSoon/' + environment.firebase.apiKey
+    );
   }
-  getInTheaters():Observable<any>{
-    return this.httpClient.get(this.baseUrl+ 'InTheaters/' + environment.apiKey)
+  getInTheaters(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'InTheaters/' + environment.firebase.apiKey
+    );
   }
-  getPopularTv():Observable<any>{
-    return this.httpClient.get(this.baseUrl+ 'MostPopularTvs/' + environment.apiKey)
+  getPopularTv(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'MostPopularTvs/' + environment.firebase.apiKey
+    );
   }
-  getPopularMovies():Observable<any>{
-    return this.httpClient.get(this.baseUrl+ 'MostPopularMovies/' + environment.apiKey)
+  getPopularMovies(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'MostPopularMovies/' + environment.firebase.apiKey
+    );
   }
-  getTop250Movies():Observable<any>{
-    return this.httpClient.get(this.baseUrl+ 'Top250Movies/' + environment.apiKey)
+  getTop250Movies(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'Top250Movies/' + environment.firebase.apiKey
+    );
   }
-  getTop250TvShows():Observable<any>{
-    return this.httpClient.get(this.baseUrl+ 'Top250TVs/' + environment.apiKey)
+  getTop250TvShows(): Observable<any> {
+    return this.httpClient.get(
+      this.baseUrl + 'Top250TVs/' + environment.firebase.apiKey
+    );
   }
 }
